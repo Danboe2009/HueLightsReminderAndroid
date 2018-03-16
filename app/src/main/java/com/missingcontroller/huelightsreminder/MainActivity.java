c41281fe0b481ef18b7ea14e1f1792cd30a5a1de
Danboe2009 <Danboe2009@gmail.com>
2018-03-16 16:47:11 -0400

    Initial Commit

---
+++ app/src/main/java/com/missingcontroller/huelightsreminder/MainActivity.java
@@ -0,0 +1,56 @@
+package com.missingcontroller.huelightsreminder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BufferedReader reader = null;
+
+        try {
+
+            // Defined URL  where to send data
+            URL url = new URL("/media/webservice/httppost.php");
+
+            // Send POST data request
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            //wr.write( data );
+            wr.flush();
+
+            // Get the server response
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+            // Read Server Response
+            while ((line = reader.readLine()) != null) {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+
+            Log.d("TEST", sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
