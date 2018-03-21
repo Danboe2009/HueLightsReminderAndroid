44b4a05c16f98ae11b2440e499fff3911b470717
Danboe2009 <Danboe2009@gmail.com>
2018-03-21 12:49:01 -0400

    Version 1.0

--- app/src/main/java/com/missingcontroller/huelightsreminder/MainActivity.java
+++ app/src/main/java/com/missingcontroller/huelightsreminder/MainActivity.java
@@ -2,6 +2,8 @@ package com.missingcontroller.huelightsreminder;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -14,6 +16,8 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -22,54 +26,78 @@ public class MainActivity extends AppCompatActivity {
 
     private static String TAG = "TEST";
     public String loc;
+    public int num;
+    public CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        countDownTimer = new CountDownTimer(300000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.wtf(TAG, "Time is " + millisUntilFinished);
+            }
+
+            @Override
+            public void onFinish() {
+                new CheckLights().execute();
+            }
+        };
+
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            //Cry about not being clicked on
+        } else if (extras.getBoolean("NotiClick")) {
+            Log.wtf(TAG, "Tagged");
+            new TurnOffLight().execute();
+        }
+
         new CheckLights().execute();
-
-        //SendNotification("Kitchen");
-
-
     }
 
     private void SendNotification(String loc) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("NotiClick", true);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         Resources r = getResources();
         Notification notification = new NotificationCompat.Builder(this)
                 .setSmallIcon(android.R.drawable.ic_menu_report_image)
                 .setContentTitle("You left a light on.")
                 .setContentText("You left the " + loc + " light on.")
+                .setContentIntent(pIntent)
                 .setAutoCancel(true)
                 .build();
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(0, notification);
-
-        new CheckLights().execute();
     }
 
-    private void Timer(final String loc) {
-        new CountDownTimer(300000, 1000) {
-
-            public void onTick(long millisUntilFinished) {
-                Log.wtf(TAG, "Time is " + millisUntilFinished);
-            }
-
-            public void onFinish() {
-                SendNotification(loc);
-            }
-        }.start();
+    private void Timer() {
+        countDownTimer.cancel();
+        countDownTimer.start();
     }
 
     class CheckLights extends AsyncTask<String, Void, Void> {
+
+        private Runnable RunNoti = new Runnable() {
+            @Override
+            public void run() {
+
+                SendNotification(loc);
+                Timer();
+            }
+        };
 
         private Runnable RunTimer = new Runnable() {
             @Override
             public void run() {
 
-                Timer(loc);
+                SendNotification(loc);
+                Timer();
             }
         };
 
@@ -121,21 +149,28 @@ public class MainActivity extends AppCompatActivity {
                                 Log.wtf(TAG, "Living Room Light is off");
                                 break;
                         }
+                        runOnUiThread(RunTimer);
                     } else {
                         switch (i + 1) {
                             case 1:
                             case 5:
                             case 2:
                                 loc = "Bedroom";
-                                runOnUiThread(RunTimer);
+                                num = 1;
+                                Log.wtf(TAG, "Bedroom Light " + (1 + i) + " is on");
+                                runOnUiThread(RunNoti);
                                 break;
                             case 3:
                                 loc = "Kitchen";
-                                runOnUiThread(RunTimer);
+                                num = 2;
+                                Log.wtf(TAG, "Kitchen Light is on");
+                                runOnUiThread(RunNoti);
                                 break;
                             case 4:
                                 loc = "Living Room";
-                                runOnUiThread(RunTimer);
+                                num = 3;
+                                Log.wtf(TAG, "Living Room Light is on");
+                                runOnUiThread(RunNoti);
                                 break;
                         }
                     }
@@ -152,4 +187,38 @@ public class MainActivity extends AppCompatActivity {
             // TODO: do something with the feed
         }
     }
+
+    class TurnOffLight extends AsyncTask<String, Void, Void> {
+
+        protected Void doInBackground(String... urls) {
+            try {
+                // Defined URL  where to send data
+                URL url = null;
+                try {
+                    url = new URL("http://192.168.1.154/api/ThbE9KYmC8DxS9C1AhdTiL6Fk3WSpTwODd0ATzOf/groups/" + num + "/action");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+                Log.wtf(TAG, "URL = " + url.toString());
+
+                HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+                httpCon.setDoOutput(true);
+                httpCon.setRequestMethod("PUT");
+                OutputStreamWriter out = new OutputStreamWriter(
+                        httpCon.getOutputStream());
+                out.write("{\"on\":false}");
+                out.close();
+
+
+                Log.e(TAG, "doInBackground: " + httpCon.getResponseMessage());
+            } catch (Exception e) {
+                Log.wtf(TAG, e.toString());
+            }
+
+
+            return null;
+        }
+    }
+
 }
